1d482748318d74b4a0109eb5bfa3ec706c1b1def
pp53564 <petra.petrovic@fer.hr>
2023-11-10 23:39:58 +0100

    malo promjene xss napada

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        // This allows inline scripts (e.g., <script>alert('XSS test');</script>)
+        source: "/(.*)",
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval';",
+          },
+        ],
+      },
+    ];
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import PrivateRoute from './PrivateRoute';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+
     <PrivateRoute>
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="script-src 'self' 'unsafe-inline' 'unsafe-eval';"
+        />
+      </Head>
       <Component {...pageProps} />
     </PrivateRoute>
   );
